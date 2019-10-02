5363b3c46e3f95a65bba60a277829f1a3392c38d
Elliot Saba <staticfloat@gmail.com>
2019-10-02 19:31:21 -0400

    Bzip2_jll build 1.0.6+1

--- src/Bzip2_jll.jl
+++ src/Bzip2_jll.jl
@@ -2,6 +2,11 @@ module Bzip2_jll
 using Pkg, Pkg.BinaryPlatforms, Pkg.Artifacts, Libdl
 import Base: UUID
 
+# We put these inter-JLL-package API values here so that they are always defined, even if there
+# is no underlying wrapper held within this JLL package.
+const PATH_list = String[]
+const LIBPATH_list = String[]
+
 # Load Artifacts.toml file
 artifacts_toml = joinpath(@__DIR__, "..", "Artifacts.toml")
 
@@ -15,11 +20,12 @@ platforms = filter(p -> isfile(joinpath(@__DIR__, "wrappers", triplet(p) * ".jl"
 # From the available options, choose the best platform
 best_platform = select_platform(Dict(p => triplet(p) for p in platforms))
 
+# Silently fail if there's no binaries for this platform
 if best_platform === nothing
-    error("Unable to load Bzip2; unsupported platform $(triplet(platform_key_abi()))")
+    @debug("Unable to load Bzip2; unsupported platform $(triplet(platform_key_abi()))")
+else
+    # Load the appropriate wrapper
+    include(joinpath(@__DIR__, "wrappers", "$(best_platform).jl"))
 end
-
-# Load the appropriate wrapper
-include(joinpath(@__DIR__, "wrappers", "$(best_platform).jl"))
 
 end  # module Bzip2_jll
--- src/wrappers/x86_64-w64-mingw32.jl
+++ src/wrappers/x86_64-w64-mingw32.jl
@@ -2,8 +2,6 @@
 export libbzip2
 
 ## Global variables
-const PATH_list = String[]
-const LIBPATH_list = String[]
 PATH = ""
 LIBPATH = ""
 LIBPATH_env = "PATH"
@@ -30,7 +28,6 @@ function __init__()
 
     # Initialize PATH and LIBPATH environment variable listings
     global PATH_list, LIBPATH_list
-
     global libbzip2_path = abspath(joinpath(artifact"Bzip2", libbzip2_splitpath...))
 
     # Manually `dlopen()` this right now so that future invocations
